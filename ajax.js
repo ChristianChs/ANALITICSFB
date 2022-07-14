17b861aa4062b3e23af8520953863504ed57e37c
christianchs <56552028+ChristianChs@users.noreply.github.com>
2022-07-14 14:17:16 -0500

    añadiendo grafico

--- ajax.js
+++ ajax.js
@@ -1,7 +1,8 @@
 console.log("correcto")
     //https://www.youtube.com/watch?v=M4LaQ3KUGOM
 document.querySelector('#boton').addEventListener('click', traerDatos);
-document.querySelector('#btnfb').addEventListener('click', linkFacebook);
+//document.querySelector('#btnfb').addEventListener('click', linkFacebook);
+document.querySelector('#botonlike').addEventListener('click', llenarGrafico);
 
 function traerDatos() {
     const xhttp = new XMLHttpRequest();
@@ -27,6 +28,57 @@ function traerDatos() {
                 `
             }
             console.log(data)
+        }
+    }
+}
+
+function llenarGrafico() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'HUARINO_posts.json', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var posteo = [];
+            var reactiontotal = [];
+            let data = JSON.parse(this.responseText);
+            for (let item of data) {
+                posteo.push(item.post_id)
+                reactiontotal.push(item.reaction_count)
+            }
+            console.log(posteo)
+            console.log(reactiontotal)
+            let res = document.querySelector('#resgraf');
+            res.innerHTML += `
+            <canvas id="grafica"></canvas>`;
+            const $grafica = document.querySelector("#grafica")
+            const etiquetas = posteo;
+            const datosVentas2020 = {
+                label: "REACCIONES DE PUBLICACIONES",
+                // Pasar los datos igualmente desde PHP
+                data: reactiontotal, // <- Aquí estamos pasando el valor traído usando AJAX
+                backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+                borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+                borderWidth: 1, // Ancho del borde
+            };
+            new Chart($grafica, {
+                type: 'line', // Tipo de gráfica
+                data: {
+                    labels: etiquetas,
+                    datasets: [
+                        datosVentas2020,
+                        // Aquí más datos...
+                    ]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }],
+                    },
+                }
+            });
         }
     }
 }
